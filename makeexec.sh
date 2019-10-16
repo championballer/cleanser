5a562791fa32915a806641eba35b76fb55cba5e0
Mateus Gabriel Müller <mateus@buster.home>
2019-10-15 23:03:20 -0300

    Made DIRECTORY as requisite field. Throw error on stderr if not informed. FILE_EXTENSION variable, if not informed, will use "sh" (most common).

--- makeexec.sh
+++ makeexec.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# Usage makeexec [filetype] [directory]
+# Usage: $ makeexec [directory] [file_extension]
 
 # VARIABLES
-TYPE="$1"
-DIRECTORY="$2"
-
-# TESTS
-[ -z "$TYPE" -o -z "$DIRECTORY" ] && echo "Usage makeexec [filetype] [directory]" && exit 1 # Is null?
-[ ! -d "$DIRECTORY" ] && echo "Directory does not exist." && exit 1
+DIRECTORY="${1:?DIRECTORY_WAS_NOT_INFORMED}" # directory to search, current directory is used as fallback
+FILE_EXTENSION="${2:-sh}" # py? sh?
 
 # EXECUTION
-find "$DIRECTORY" -iname "*.$TYPE"  \
+find "$DIRECTORY" -iname "*.$FILE_EXTENSION" \
 									-exec chmod +x {} \;
