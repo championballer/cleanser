a619a4241589a349cd756dd65506fdce2a041aa9
Mateus Gabriel Müller <mateus@buster.home>
2019-10-17 23:44:03 -0300

    Commented.

--- clean_trash.sh
+++ clean_trash.sh
@@ -4,11 +4,13 @@ COLOR_GREEN="\033[1;32m"
 COLOR_BLUE="\033[0;34m"
 COLOR_RED="\033[1;31m"
 COLOR_NONE='\033[0m'
+
+# How many folders found?
 COUNT_TRASH_FOLDERS_FOUND=$(find "$HOME" -iname trash \
                                          -type d \
                                          2> /dev/null | \
                                          wc -l)
-
+# Print folders found
 _print_directories_found () {
   printf "${COLOR_RED}ATTENTION! $COUNT_TRASH_FOLDERS_FOUND folders found:${COLOR_NONE}\n\n"
   local option=1
@@ -19,6 +21,7 @@ _print_directories_found () {
   printf "\n"
 }
 
+# Receive $1 which is the line number to keep the output from find command
 _remove_files_specific_directory_selected () {
   find "$HOME" -iname trash \
                -type d \
@@ -27,6 +30,7 @@ _remove_files_specific_directory_selected () {
                xargs rm -rf 1> /dev/null 2>&1
 }
 
+# Remove files (without sed described above)
 _remove_files_only_directory () {
   find "$HOME" -iname trash \
                -type d \
@@ -34,10 +38,10 @@ _remove_files_only_directory () {
                xargs rm -rf 1> /dev/null 2>&1
 }
 
-if [ $COUNT_TRASH_FOLDERS_FOUND -gt 1 ]; then
-  _print_directories_found
-  read -p "Type the number of the correct Trash folder: " folder_number
-  _remove_files_specific_directory_selected $folder_number
+if [ $COUNT_TRASH_FOLDERS_FOUND -gt 1 ]; then # More than one directory found?
+  _print_directories_found # Print directories
+  read -p "Type the number of the correct Trash folder: " folder_number # Ask the user which directory
+  _remove_files_specific_directory_selected $folder_number # Sed line to keep
 else
   _remove_files_only_directory
 fi
