885c11dfcc65f98e79fa4f5721440a7ba121f175
Mateus Gabriel Müller <mateus@buster.home>
2019-10-17 21:48:38 -0300

    Changed makeexec

--- makeexec.sh
+++ makeexec.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-#usage makeexec [filetype]
+# Usage: $ makeexec [directory] [file_extension]
 
-find . -type f -name "*$1" -exec chmod +x {} \; 
+# VARIABLES
+DIRECTORY="${1:?DIRECTORY_WAS_NOT_INFORMED}" # throw error if not informed
+FILE_EXTENSION="${2:-sh}" # py? sh?
+
+# EXECUTION
+find "$DIRECTORY" -iname "*.$FILE_EXTENSION" \
+                  -exec chmod +x {} \;
